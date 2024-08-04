2ed205df4e8a41da671e0a7bb82eb47ac2202302
Ken Wood <ken@roarmarketingconcepts.com>
2024-08-03 22:57:19 -0400

    Add files via upload

--- pycode/main.py
+++ pycode/main.py
@@ -1,6 +1,6 @@
 from langchain.llms import OpenAI 
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv  
 import argparse
 import os
@@ -11,23 +11,55 @@ load_dotenv()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--language', type=str, default='Python')
-parser.add_argument('--task', type=str, default='return a list of prime numbers')
+parser.add_argument('--task', type=str, default='determine value of pi to ten decimal places')
 args=parser.parse_args()
 
 llm = OpenAI()
+
+#  Prompts
 
 code_prompt = PromptTemplate(
     template="Write a short {language} function that will {task}.",
     input_variables=["language", "task"]
     )
+
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} code:\n{code}",
+    input_variables=["language", "task"]
+    )
+
+# Chains
+
 code_chain = LLMChain(
     llm=llm, 
-    prompt=code_prompt
+    prompt=code_prompt,
+    output_key="code"
     )   
 
-result = code_chain({
+test_chain = LLMChain(
+    llm=llm, 
+    prompt=test_prompt,
+    output_key="test"
+    )
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+    )
+
+result = chain({
     "language": args.language, 
     "task": args.task
     })
-print(result)
-print(result["text"])   
+
+# result = code_chain({
+#     "language": args.language, 
+#     "task": args.task
+#     })
+
+print(">>>>>>>>GENERATED CODE<<<<<<<<")
+print(result["code"])
+print(">>>>>>>>GENERATED TEST<<<<<<<<")
+print(result["test"])
+ 
