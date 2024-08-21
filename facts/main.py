fd95a7f61a434bcb7841821df918bf408f09c151
Ken Wood <ken@roarmarketingconcepts.com>
2024-08-21 11:32:50 -0400

    Add files via upload

--- facts/main.py
+++ facts/main.py
@@ -1,13 +1,29 @@
-from langchain.document_loaders import TextLoader
-# from langchain.chat_models import ChatOpenAI
-# from langchain.chains import LLMChain
-# from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
-# from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-loader = TextLoader("facts.txt")
-docs = loader.load()
+embeddings = OpenAIEmbeddings()
 
-print(docs)
+emb = embeddings.embed_query("hello")
+print(emb)
+
+# text_splitter = CharacterTextSplitter(
+#     separator="\n",
+#     chunk_size=200,
+#     chunk_overlap=0
+#     )
+
+# loader = TextLoader("facts.txt")
+
+# docs = loader.load_and_split(
+#     text_splitter=text_splitter
+# )
+
+# for doc in docs:
+#     print(doc.page_content)
+#     print("\n")
+
