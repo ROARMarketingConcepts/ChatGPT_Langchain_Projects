0765854bfedb5aca3969d1dd8a1617fbc6f8c2ff
Ken Wood <ken@roarmarketingconcepts.com>
2024-08-23 08:38:37 -0400

    Add files via upload

---
+++ facts/prompt.py
@@ -0,0 +1,26 @@
+from langchain.vectorstores import Chroma
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+embeddings = OpenAIEmbeddings()
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings
+)
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever=retriever,
+    chain_type="stuff"
+)
+
+result = chain.run("What is an interesting fact about the English language?")
+
+print(result)
