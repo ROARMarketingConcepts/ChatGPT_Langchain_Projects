37fc1138950ce58239bd43ecbfae83b0a6b0eba0
Ken Wood <ken@roarmarketingconcepts.com>
2024-08-27 16:27:15 -0400

    Add files via upload

--- agents/main.py
+++ agents/main.py
@@ -1,14 +1,19 @@
 from langchain.chat_models import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain.schema import SystemMessage
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate,MessagesPlaceholder
 from langchain.agents import OpenAIFunctionsAgent,AgentExecutor
 from dotenv import load_dotenv
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool # import the functions from tools/sql.py
 
 load_dotenv()
 
 chat = ChatOpenAI()
+tables = list_tables()  # get the list of tables from the database
+# print(tables)
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content=f"You are an AI that has access to a SQLite database.\n{tables}"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")   # this is where the agent will store intermediate AI assistant                                                      
     ]                                                           # and function messages. It is a scratchpad for the agent.
@@ -19,7 +24,7 @@ prompt = ChatPromptTemplate(
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
-    tools=[run_query_tool],
+    tools=[run_query_tool,describe_tables_tool],
     prompt=prompt
     
 )
@@ -31,7 +36,7 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=[run_query_tool]
+    tools=[run_query_tool,describe_tables_tool]
 )
 
-agent_executor("How many users provide their email?") 
+agent_executor("How many users have a shipping address?") 
--- agents/tools/sql.py
+++ agents/tools/sql.py
@@ -4,19 +4,42 @@ from langchain.tools import Tool
 # Establish connection to SQLite database
 conn = sqlite3.connect("db.sqlite")
 
+def list_tables():
+    """List all tables in the database."""
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = cursor.fetchall()   
+    return "\n".join([row[0] for row in rows if row[0] is not None])   # return the table names as a string
+
 
 def run_sqlite_query(query):
     """Execute a SQL query and return the results."""
     cursor = conn.cursor()
-    cursor.execute(query)
-    return cursor.fetchall()
-
+    try:
+        cursor.execute(query)
+        return cursor.fetchall()
+    except sqlite3.OperationalError as err:     # error handling
+        return f"Error executing query: {str(err)}"
+    
 # Create a Tool for executing SQLite queries.
     
 run_query_tool = Tool.from_function(
     name="run_sqlite_query",
     description="Run a SQLite query on the connected database.",
     func=run_sqlite_query,
+)
+
+def describe_tables(table_names):
+    """Get the schema of a specific table in the database."""
+    cursor = conn.cursor()
+    tables = ', '.join("'"+table+ "'" for table in table_names)
+    rows = cursor.execute(f"SELECT sql FROM sqlite_master WHERE type= 'table' AND name IN ({tables});")
+    return "\n".join([row[0] for row in rows if row[0] is not None])   # return the table names as a stringrows
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given a list of table names, returns the schema of those tables.",
+    func=describe_tables,
 )
 
 # # Create a Tool for getting table names
