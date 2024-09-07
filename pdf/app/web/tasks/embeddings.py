6984e291dca58787dae6d95942c370658b9af0d9
Ken Wood <ken@roarmarketingconcepts.com>
2024-09-07 16:26:24 -0400

    Add files via upload

--- pdf/app/web/tasks/embeddings.py
+++ pdf/app/web/tasks/embeddings.py
@@ -8,5 +8,5 @@ from app.chat import create_embeddings_for_pdf
 @shared_task()    # This decorator makes the function a Celery task which runs in the background.
 def process_document(pdf_id: int):
     pdf = Pdf.find_by(id=pdf_id)
-    with download(pdf.id) as pdf_path:
-        create_embeddings_for_pdf(pdf.id, pdf_path)
+    with download(pdf_id) as pdf_path:
+        create_embeddings_for_pdf(pdf_id, pdf_path)
